b7d4bc56165ffcf3503217f6dea166601041a69b
Satyabrata Behera <98028025+satya-thecoder@users.noreply.github.com>
2022-01-29 13:38:12 +0530

    Update LeapYear.java

--- ifelse/LeapYear.java
+++ ifelse/LeapYear.java
@@ -1,8 +1,19 @@
-import java.lang.*;
-class LeapYear
+import java.io.*;
+import java.util.*;
+class Leapyear
 {
-public static void main(String[] args)
-{
-System.out.println("hii");
-}
+    public static void main(String[] args)
+    {
+        int y;
+    Scanner sc=new Scanner(System.in);
+        System.out.println("enter year:");
+        y=sc.nextInt();
+        if(y!=0 && y%4==0)
+        {
+            System.out.println("year is leaf year");
+        }
+        else{
+            System.out.println("year is not leap year");
+        }
+    }
 }
